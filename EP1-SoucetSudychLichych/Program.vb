bcc9fd312c0c3875967cdc2a2abf9f8a9c9383d4
dan <danik.fiala@email.cz>
2024-06-05 16:40:03 +0200

    začátek ničeho

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -2,6 +2,16 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+        Dim cislo As Integer = 1
+
+        Console.WriteLine(" Zadej vhodné èíslo ")
+        cislo = Console.ReadLine
+
+
+        If cislo < 1 Then
+            Console.WriteLine(" Minimální zadané èíslo musí být 1")
+            Console.ReadKey()
+        End If
+
+    End Sub
 End Module
